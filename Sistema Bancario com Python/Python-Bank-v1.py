d9b3037bba15c167769ad0e55b070aea33f66fd7
Anderson <andersonsramalho@outlook.com>
2023-08-21 21:09:05 -0300

    Finalizacao codigo sistema bancário básico

--- Sistema Bancario com Python/Python-Bank-v1.py
+++ Sistema Bancario com Python/Python-Bank-v1.py
@@ -13,6 +13,7 @@ saldo = float(0)
 limite = float(500)
 extrato = ""
 numero_saques = 0
+numero_depositos = 0
 LIMITE_SAQUES = 3
 
 while True:
@@ -20,26 +21,47 @@ while True:
     opcao = input(menu)
 
     if opcao == "d":
-        print("Depósito")
+        print("\n============ Depósito ===========\n")
         valor_deposito = float(input("Digite o valor do depósito: R$ "))
 
         if valor_deposito <= 0:
             print("Valores zero ou negativos não são permitidos, tente novamente")
         else:
             saldo += valor_deposito
-            print(f"Depósito realizado com sucesso, o saldo atualizado da sua conta é de R$ {saldo}")
-            retorno = input("deseja retornar para o menu? y/n ")
-            
-            if retorno == "n":
-                nova_operacao = input("deseja repetir a operação? y/n ")
-                
-            
+            numero_depositos += 1
+            extrato += f"Depósito {numero_depositos}: R$ {valor_deposito:.2f}\n"
+
+            print(f"Depósito realizado com sucesso, o saldo atualizado da sua conta é de R$ {saldo:.2f}\n")
+            print("\n==============================\n")
 
     elif opcao == "s":
-        print("Sacar")
+        if numero_saques >= LIMITE_SAQUES:
+            print("Limite de saques atingido, operação cancelada")
+        else:    
+            print("\n============ Sacar ===========\n")
+            valor_saque = float(input("Digite o valor do saque: R$ "))
+
+            if valor_saque <= 0:
+                print("Valores zero ou negativos não são permitidos, tente novamente")
+            elif valor_saque > saldo:
+                print(f"Saque maior que saldo atual da conta de R$ {saldo:.2f}, tente novamente conforme saldo disponível")
+            elif valor_saque > 500:
+                print("Saque acima de R$ 500.00 não permitido em uma única transação, tente novamente")
+            elif numero_saques >= LIMITE_SAQUES:
+                print("Limite de saques atingido, operação cancelada")
+            else:
+                numero_saques += 1
+                saldo -= valor_saque
+                extrato += f"Saque {numero_saques}: R$ -{valor_saque:.2f}\n"
+                print(f"Saque realizado com sucesso, saldo disponível atualizado é de R$ {saldo:.2f}\n")
+                print("\n==============================\n")
 
     elif opcao == "e":
-        print("Extrato")
+        print("\n===================== Extrato ==============\n")
+        print(extrato)
+        print(f"Seu Saldo atual é de R$ {saldo:.2f}\n")
+        print("\n==============================\n")
+
 
     elif opcao == "q":
         print("Sessão Finalizada")
