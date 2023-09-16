72045e1f7981f9328a630423c4afce89904a84c3
Anderson <andersonsramalho@outlook.com>
2023-09-16 11:55:02 -0300

    Finalização do codigo para leitura de XMLs

--- Projetos-Reais/Leitor_XMLs.py
+++ Projetos-Reais/Leitor_XMLs.py
@@ -1,33 +1,58 @@
 import xmltodict
 import os
-import json
+import pandas as pd
 
-
-def pegar_infos(nome_arquivo):
-    print(f"Pegou as informações {nome_arquivo}")
+def pegar_infos(nome_arquivo, valores):
+    # print(f"Pegou as informações {nome_arquivo}")
     with open(f'C:/Users/Anderson/OneDrive/Documentos/XMLs_NFe/{nome_arquivo}', "rb") as arquivo_xml:
         dic_arquivo = xmltodict.parse(arquivo_xml)
-        try:
-            infos_nf = dic_arquivo["nfeProc"]["NFe"]['infNFe']
-            numero_nfe = infos_nf['ide']['nNF']
-            data_emissao = infos_nf['ide']['dhEmi']
-            empresa_emissora = infos_nf['emit']['xNome']
-            CNPJ_emissor = infos_nf['emit']['CNPJ']
-            nome_cliente = infos_nf['dest']['xNome']
-            CNPJ_cliente = infos_nf['dest']['CNPJ']
-            # valor_nfe = infos_nf['total']['vProd']
-            # qtde_nfe = infos_nf['transp']['vol']['qvol']
-            # um_nfe = infos_nf['transp']['vol']['esp']
+
+        infos_nf = dic_arquivo["nfeProc"]["NFe"]['infNFe']
+        numero_nfe = infos_nf['ide']['nNF']
+        data_emissao = infos_nf['ide']['dhEmi']
+        empresa_emissora = infos_nf['emit']['xNome']
+        CNPJ_emissor = infos_nf['emit']['CNPJ']
+        nome_cliente = infos_nf['dest']['xNome']
+        CNPJ_cliente = infos_nf['dest']['CNPJ']
+
+        if "NFref" in infos_nf['ide']:
             nf_referenciada = infos_nf['ide']['NFref']['refNFe']
-            print(numero_nfe, empresa_emissora, nf_referenciada, sep="\n")
-        except Exception as e:
-            print(e)
-            # print(json.dumps(dic_arquivo, indent=4))
+        else:
+            nf_referenciada = "Sem NF Referencia"
+        
+        valor_nfe = infos_nf["total"]["ICMSTot"]["vProd"]
 
+        if "vol" in infos_nf["transp"]:
+            qtde_nfe = infos_nf['transp']['vol']['qVol']
+        else:
+            qtde_nfe = "Qtde NF não encontrado"
 
+        if "vol" in infos_nf["transp"]:
+            um_nfe = infos_nf['transp']['vol']['esp']
+        else:
+            um_nfe = "UM não encontrada"
+
+        valores.append([numero_nfe, data_emissao, empresa_emissora, 
+                       CNPJ_emissor, nome_cliente, CNPJ_cliente, 
+                       nf_referenciada, valor_nfe, 
+                       qtde_nfe, um_nfe]
+                       )
 
 lista_arquivos = os.listdir("C:/Users/Anderson/OneDrive/Documentos/XMLs_NFe")
 
+colunas = ["numero_NFe", "Data_emissao", "Emissor", 
+           "CNPJ_Emissor", "Destinatario", "CNPJ_Dest", 
+           "NF_Referenciada", "Valor_NFe", "Qtde_NFe", 
+           "UM_NFe"
+           ]
+
+valores = []
+
 for arquivo in lista_arquivos:
-    pegar_infos(arquivo)
-    break
+    pegar_infos(arquivo, valores)
+    
+tabela = pd.DataFrame(columns=colunas, data=valores)
+tabela.to_excel(f"C:/Users/Anderson/OneDrive/Documentos/XMLs_NFe/NFDs.xlsx", index=False)
+
+
+# print(tabela)
